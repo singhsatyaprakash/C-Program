2e46b839354c619a5640191ab9f04afe42a92b84
kingofmidgard <satyaprakashsingh70675@gmail.com>
2024-08-23 21:04:57 +0530

    C programs

--- 2-D_ARRAY/transpose.c
+++ 2-D_ARRAY/transpose.c
@@ -16,15 +16,15 @@ int main() {
          scanf("%d",&matrix[i][j]);
         }
     }
-        printf("\n");
-        for(i=0;i<r;i++)
-    {
-        for(j=0;j<c;j++)
-        {
-         printf("%d  ",matrix[i][j]);
-        }
-        printf("\n");
-    }
+    //     printf("\n");
+    //     for(i=0;i<r;i++)
+    // {
+    //     for(j=0;j<c;j++)
+    //     {
+    //      printf("%d  ",matrix[i][j]);
+    //     }
+    //     printf("\n");
+    // }
         printf("\n");
     // transopse of matrix changing row to columns wise versa...
          for(i=0;i<c;i++){
